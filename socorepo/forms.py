c0880d3ad55c9d8f8aab53f5cc16600661c10c2b
LoadingByte <hello@loadingbyte.com>
2021-05-10 23:09:41 +0200

    Fix missing localization of "Any" qualifier

--- socorepo/forms.py
+++ socorepo/forms.py
@@ -11,7 +11,8 @@ def create_component_filter_form(available_qualifiers):
     class ComponentFilterForm(FlaskForm):
         version = StringField(_("project.filter_version"))
         qualifier = SelectField(_("project.filter_qualifier"),
-                                choices=[("", "Any")] + [(q.name, q.name) for q in available_qualifiers],
+                                choices=[("", _("project.filter_qualifier_any"))] +
+                                        [(q.name, q.name) for q in available_qualifiers],
                                 default=default_qualifier.name if default_qualifier in available_qualifiers else "")
         filter = SubmitField(_("project.filter"))
 
